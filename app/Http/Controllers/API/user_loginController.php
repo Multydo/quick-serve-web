6a69e27ce94284b7575d626ab0d2775f766e4eb3
Nassim <57188970+nassim-stack@users.noreply.github.com>
2024-09-05 22:40:16 +0300

    login done

--- app/Http/Controllers/API/user_loginController.php
+++ app/Http/Controllers/API/user_loginController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\User;
+
 class user_loginController extends Controller
 {
     public function user_login(Request $request){
         $credentials = $request->only("email","password");
         if(Auth::attempt($credentials)){
-            return response()->json("authentication successful");
+            $useremail=$request["email"];
+      
+            $userrole=User::where("email",$useremail)->first();
+            
+            return response()->json(["role"=>$userrole["role"]]);
         }
 }
 }
